4b9351294d0567c1fa72ebfb650c3ee55ccc9549
Evgastap <Evgastap@gmail.com>
2021-05-29 23:22:12 +0200

    Updated `Programming.js` with react-typical

--- src/Programming.js
+++ src/Programming.js
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion'
 import React from 'react'
 import ProgressiveImage from 'react-progressive-image'
+import Typical from 'react-typical';
 
 // styles for the programming image
 const progImgStyle = {
@@ -12,8 +13,28 @@ const progImgStyle = {
 
 export default function Programming() {
     return (
-        <div className="h-screen">
-            <ProgressiveImage src="/programming.jpg" delay={1500} placeholder="/programming_mini.jpg">{src => <motion.img layoutId="programmingHero" src={src} className="h-full w-full" alt="Programming Evgeny" />}</ProgressiveImage>
+        <div className="grid grid-cols-1 grid-rows-2 sm:grid-cols-2 sm:grid-rows-1 w-full flex justify-center items-center text-center text-2xl text-mono">
+            <ProgressiveImage src="/programming.jpg" delay={1500} placeholder="/programming_mini.jpg">
+                {src => <motion.img layoutId="programmingHero" src={src} className="w-full" alt="Programming Evgeny" />}
+            </ProgressiveImage>
+            <Typical
+                steps={
+                    [
+                        'Hello!', 1000, 
+                        "I'm Evgeny 👋", 1500,
+                        "I...", 500,
+                        "I sometimes write code.", 1500,
+                        "I can code in Java", 1500,
+                        "I can code in spaghetti 🍝", 500,
+                        "I can code in R", 1500,
+                        "I coded this site in React + Tailwind", 1500,
+                        "I have impostor syndrome?", 1500, 
+                        "I can `git push --force`", 1500
+                    ]
+                }
+                loop={Infinity}
+                wrapper="p"
+            />
         </div>
     )
 }
