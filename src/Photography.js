a28316521729d084dc005e892f298accfc732c3b
Evgastap <Evgastap@gmail.com>
2021-06-08 09:31:32 +0200

    margin & alt changes

--- src/Photography.js
+++ src/Photography.js
@@ -37,13 +37,14 @@ class Photography extends React.Component {
                     <motion.div layoutId="photographyHero" alt="Photography Evgeny" className="object-cover min-h-full w-full -z-10" style={photoImgStyle} />
                     <h1 className="absolute">I ❤ taking pictures.</h1>
                 </div>
-                <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 mt-10 mx-5">
+
+                <div className="grid grid-cols-1 sm:grid-cols-4 gap-4 my-5 mx-5">
                     {
                         listOfImages.map(
                             // hey, I'm literally taking images from a directory and applying a class name if they have a 'col-span-x' property in their name.
                             // who needs a CMS, right?
                             (image, index) => <div className={`w-full h-full ${image.default.includes('col-span-2') && "sm:col-span-2" || image.default.includes('col-span-3') && "sm:col-span-3"}`}>
-                                <img key={index} src={image.default} alt="info" className="object-cover min-h-full"></img>
+                                <img key={index} src={image.default} alt={`Evgeny Astapov Photo Portfolio ${index + 1}`} className="object-cover min-h-full"></img>
                             </div>
                         )
                     }
