dd2909c27ea35c2d8844f0f08314d2ff7916ef13
Evgastap <Evgastap@gmail.com>
2021-06-13 21:41:55 +0200

    randomized roasts, added git avatar, mobile fixes

--- src/Programming.js
+++ src/Programming.js
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
 import React, { useState } from "react"
 import Typical from "react-typical"
 import CardWithText from "./elements/CardWithText"
@@ -15,6 +15,7 @@ const progImgStyle = {
 
 export default function Programming() {
   const [username, setUsername] = useState("")
+  const [gitPictureUrl, setGitPictureUrl] = useState("");
   const [roasts, setRoasts] = useState([])
   const [currentRoastIndex, setCurrentRoastIndex] = useState(0)
 
@@ -30,6 +31,7 @@ export default function Programming() {
         .get(`https://api.github.com/users/${username}`)
         .then((res) => {
           roastGit(res.data)
+          setGitPictureUrl(res.data.avatar_url)
         })
         .catch((err) => console.log(err))
 
@@ -47,13 +49,15 @@ export default function Programming() {
 
   const roastGit = (data) => {
     const addedRoasts = []
+
     addedRoasts.push(`${data.public_repos} public repos? So much for collaborating on code huh`)
+    addedRoasts.push(`${data.followers} followers? My grandma has more followers on ig`)
+
     if (data.public_gists === 0) {
       addedRoasts.push("0 public gists? Do you even know what that is?")
     } else {
       addedRoasts.push(`Damn, you have ${data.public_gists} gists? Who even uses gists`)
     }
-    addedRoasts.push(`${data.followers} followers? Pathetic`)
 
     if (data.bio === null) {
       addedRoasts.push("Programming so much that you don't even have a git bio?")
@@ -80,6 +84,8 @@ export default function Programming() {
       addedRoasts.push("Doesn't every dev flex their twitter in their github bio?")
     }
 
+    addedRoasts.sort(() => Math.random() - 0.5)
+    addedRoasts.push("Ok I've ran out of roasts... Maybe go to my git repo and add some more? ;)")
     setRoasts(addedRoasts)
   }
 
@@ -161,9 +167,9 @@ export default function Programming() {
         />
       </div>
 
-      <h2 className="text-4xl w-full text-center my-10">But... Can Evg backend?</h2>
+      <h2 className="text-2xl sm:text-4xl w-full text-center mt-5 sm:my-10">But... Can Evg backend?</h2>
 
-      <section className="flex flex-col max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 md:flex-row md:h-48">
+      <section className="flex flex-col max-w-4xl mx-auto sm:my-5 mb-5 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 md:flex-row md:h-48">
         <div className="md:flex md:items-center md:justify-center md:w-1/2 md:bg-gray-700 md:dark:bg-gray-800">
           <div className="px-6 py-6 md:px-8 md:py-0">
             {roasts.length === 0 && (
@@ -179,25 +185,27 @@ export default function Programming() {
 
             <p className="mt-2 text-sm text-gray-600 dark:text-gray-400 md:text-gray-200 text-justify">
               {roasts.length === 0
-                ? "How <b>dare</b> you assume that I can't backend? I'm not amazing at web-related backend, but I'm still trying to imporve. This is why I introduce to you... Roast my git!"
+                ? <span>How <b>dare</b> you assume that I can't backend? I mean, I'm not amazing at web-related backend, but I'm still trying to imporve. This is why I introduce to you... Roast my git!</span>
                 : roasts[currentRoastIndex]}
             </p>
           </div>
         </div>
 
-        <div className="align-middle">
+        <div className="m-auto align-middle">
           <div className="w-full flex items-center justify-center mb-5">
-            <svg width="64" height="64" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
-                transform="scale(64)"
-                fill="#4b5563"
-              />
-            </svg>
+              {roasts.length === 0 ? <svg width="64" height="64" viewBox="0 0 1024 1024" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <motion.path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M8 0C3.58 0 0 3.58 0 8C0 11.54 2.29 14.53 5.47 15.59C5.87 15.66 6.02 15.42 6.02 15.21C6.02 15.02 6.01 14.39 6.01 13.72C4 14.09 3.48 13.23 3.32 12.78C3.23 12.55 2.84 11.84 2.5 11.65C2.22 11.5 1.82 11.13 2.49 11.12C3.12 11.11 3.57 11.7 3.72 11.94C4.44 13.15 5.59 12.81 6.05 12.6C6.12 12.08 6.33 11.73 6.56 11.53C4.78 11.33 2.92 10.64 2.92 7.58C2.92 6.71 3.23 5.99 3.74 5.43C3.66 5.23 3.38 4.41 3.82 3.31C3.82 3.31 4.49 3.1 6.02 4.13C6.66 3.95 7.34 3.86 8.02 3.86C8.7 3.86 9.38 3.95 10.02 4.13C11.55 3.09 12.22 3.31 12.22 3.31C12.66 4.41 12.38 5.23 12.3 5.43C12.81 5.99 13.12 6.7 13.12 7.58C13.12 10.65 11.25 11.33 9.47 11.53C9.76 11.78 10.01 12.26 10.01 13.01C10.01 14.08 10 14.94 10 15.21C10 15.42 10.15 15.67 10.55 15.59C13.71 14.53 16 11.53 16 8C16 3.58 12.42 0 8 0Z"
+                  transform="scale(64)"
+                  fill="#4b5563"
+                />
+              </svg>
+              : 
+              <motion.img src={gitPictureUrl} className="rounded-full" style={{width: '64px', height: '64px'}} />}
           </div>
-          <div className="flex items-center justify-center pb-6 md:py-0 w-full mx-5">
+          <div className="flex items-center justify-center pb-6 md:py-0 w-full sm:mx-5">
             <form onSubmit={handleSubmit}>
               <div className="flex flex-col overflow-hidden border rounded-lg dark:border-gray-600 lg:flex-row">
                 <input
@@ -210,7 +218,7 @@ export default function Programming() {
                   onChange={handleChange}
                 />
                 <button className="px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-200 transform bg-gray-700 hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
-                  Roast me
+                  {roasts.length === 0 ? "Roast me" : "Again"}
                 </button>
               </div>
             </form>
