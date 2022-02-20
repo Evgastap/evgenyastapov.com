5d3ce16dab0347fa652bec6d1f73d1ea97256efe
Evgeny <evgastap@gmail.com>
2022-02-20 20:48:20 +0100

    solid progress, front page works, so does /photography

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
-import type { value AppProps } from "next/app";
-import { value AnimateSharedLayout, AnimatePresence } from "framer-motion";
+import { AppProps } from "next/app";
+import { AnimateSharedLayout, AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
