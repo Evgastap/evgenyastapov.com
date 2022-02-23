44f74db7dad6251f1df80d32320d4d471c03a2eb
Evgeny <evgastap@gmail.com>
2022-02-23 11:00:25 +0100

    need to read documentation first

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // https://github.com/vercel/next.js/issues/21079
   // Remove this workaround whenever the issue is fixed
   images: {
-    loader: "cloudflare",
+    loader: "custom",
     path: "/",
   },
 };
