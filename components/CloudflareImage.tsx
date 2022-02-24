06be189425ca353b8f685ecdaa29e10bae5f063f
Evgeny <evgastap@gmail.com>
2022-02-24 20:49:14 +0100

    reverted cf site url

--- components/CloudflareImage.tsx
+++ components/CloudflareImage.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 const workerDomain = "https://next-images.evgastap.workers.dev";
-const cloudflareDomain = process.env.SITE_URL;
+const cloudflareDomain = "https://evgenyastapov-com.pages.dev";
 
 // replace [yourprojectname] and [yourdomain.com] with your actual project name and (custom) domain
 const cloudflareImageLoader = ({ src, width, quality }) => {
