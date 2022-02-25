1923a0a011a1f6ec656e3bc78fa155bb8ec9e873
Evgeny <evgastap@gmail.com>
2022-02-25 14:10:17 +0100

    sitemap: changed URL, out directory

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://example.com",
+  siteUrl: "https://evgenyastapov.com",
   generateRobotsTxt: true, // (optional)
+  outDir: "out"
   // ...other options
 };
