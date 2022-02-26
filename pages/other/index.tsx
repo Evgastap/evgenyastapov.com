a3be3d853d9e18a6e5a56f6503ed16ab006a427e
Evgeny <evgastap@gmail.com>
2022-02-26 09:32:47 +0100

    noindexed WIP pages

--- pages/other/index.tsx
+++ pages/other/index.tsx
@@ -2,24 +2,28 @@ import { motion } from "framer-motion";
 import React from "react";
 import * as constants from "../../constants";
 import CloudflareImage from "../../components/CloudflareImage";
+import { NextSeo } from "next-seo";
 
 export default function Other() {
   return (
-    <div
-      className="flex h-screen
+    <>
+    <NextSeo noindex={true} />
+      <div
+        className="flex h-screen
                             w-full items-center justify-center overflow-hidden 
                             bg-gradient-to-bl from-yellow-500 text-center font-roboto text-2xl
                             font-extralight 
                             text-white sm:text-4xl"
-    >
-      <motion.div
-        layoutId="otherHero"
-        transition={constants.TRANSITION_SETTINGS}
-        className="relative -z-10 h-full w-full object-cover"
       >
-        <CloudflareImage src="/other.jpg" layout="fill" objectFit="cover" />
-      </motion.div>
-      <p className="absolute mx-2 text-white">Page under construction 👀</p>
-    </div>
+        <motion.div
+          layoutId="otherHero"
+          transition={constants.TRANSITION_SETTINGS}
+          className="relative -z-10 h-full w-full object-cover"
+        >
+          <CloudflareImage src="/other.jpg" layout="fill" objectFit="cover" />
+        </motion.div>
+        <p className="absolute mx-2 text-white">Page under construction 👀</p>
+      </div>
+    </>
   );
 }
--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -2,24 +2,28 @@ import { motion } from "framer-motion";
 import React from "react";
 import * as constants from "../../constants";
 import CloudflareImage from "../../components/CloudflareImage";
+import { NextSeo } from "next-seo";
 
 export default function Work() {
   return (
-    <div
-      className="flex h-screen
+    <>
+      <NextSeo noindex={true} />
+      <div
+        className="flex h-screen
                             w-full items-center justify-center overflow-hidden 
                             bg-gradient-to-bl from-pink-500 text-center font-roboto text-2xl
                             font-extralight 
                             text-white sm:text-4xl"
-    >
-      <motion.div
-        layoutId="workHero"
-        transition={constants.TRANSITION_SETTINGS}
-        className="relative -z-10 h-full w-full object-cover"
       >
-        <CloudflareImage src="/work.jpg" layout="fill" objectFit="cover" />
-      </motion.div>
-      <p className="absolute mx-2 text-white">Page under construction 👀</p>
-    </div>
+        <motion.div
+          layoutId="workHero"
+          transition={constants.TRANSITION_SETTINGS}
+          className="relative -z-10 h-full w-full object-cover"
+        >
+          <CloudflareImage src="/work.jpg" layout="fill" objectFit="cover" />
+        </motion.div>
+        <p className="absolute mx-2 text-white">Page under construction 👀</p>
+      </div>
+    </>
   );
 }
