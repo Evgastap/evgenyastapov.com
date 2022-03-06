1ceb339aa85247b94874641d175b1e6047a3f7a4
Evgeny <evgastap@gmail.com>
2022-03-06 10:48:40 +0100

    text on 404 displays correctly

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,13 +3,15 @@ import CloudflareImage from "../components/CloudflareImage";
 export default function Custom404() {
   return (
     <>
-      <div className="relative -z-10 mx-4 flex h-screen w-screen flex-col items-center	 justify-center text-white">
-        {/* <h1 className="font-handwriting text-5xl">
+      <div className="absolute z-10 text-center h-screen w-screen flex items-center flex-col justify-center text-white px-4">
+        <h1 className="font-handwriting text-5xl">
           That's awkward. This page doesn't exist.
         </h1>
         <h2 className="mt-2 text-2xl font-light">
           Have a cool picture instead.
-        </h2> */}
+        </h2>
+      </div>
+      <div className="flex-coltext-white relative -z-10 h-screen w-screen">
         <CloudflareImage
           src="/Northern lights.jpg"
           layout="fill"
