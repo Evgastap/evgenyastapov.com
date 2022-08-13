e2fe6d65aa44d2a527690e714d97436ef40583a0
Evgastap <evgastap@gmail.com>
2022-08-13 19:42:30 +0200

    actually installed sentry

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const { withSentryConfig } = require('@sentry/nextjs');
+
+moduleExports = {
   // https://github.com/vercel/next.js/issues/21079
   // Remove this workaround whenever the issue is fixed
   images: {
@@ -6,3 +8,17 @@ module.exports = {
     path: "https://res.cloudinary.com/evgenyastapov-com/image/upload/",
   },
 };
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
